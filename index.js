56e46f4f30921f583043af165a78b851c11d1d09
Marissa Zhong <mwz2101@columbia.edu>
2018-07-18 20:59:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,8 +26,9 @@ class Trip {
   constructor(driver, passenger) {
     this.id = ++tripId;
     this.driverId = driver.id;
-    this.passengerId = passenger.id;
-
+    if(passenger){
+      this.passengerId = passenger.id;
+    }
     store.trips.push(this);
   }
   setPassenger(passenter) {
