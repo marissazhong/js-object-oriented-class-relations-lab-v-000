e4b4b3de9486ac7dfec8973e520bd7f35fb3e31d
Marissa Zhong <mwz2101@columbia.edu>
2018-07-18 21:04:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ class Driver {
     this.name = name;
     store.drivers.push(this);
   }
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId === this.id;
+    })
+  }
 }
 
 let passengerId = 0;
